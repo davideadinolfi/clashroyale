d11fcad2d48dc751d9dd6b49a9fa7b377e1e0540
davideadinolfi <davideadinolfi01@gmail.com>
2025-12-22 17:20:03 +0100

    2

--- scripts/yolo_train.py
+++ scripts/yolo_train.py
@@ -4,10 +4,34 @@ model = YOLO('yolov8n.pt')
 
 # Addestramento
 model.train(
-    data='../dataset/1/data.yaml',  # percorso al file yaml
-    epochs=100,
+    data='../dataset/2/data.yaml',  # percorso al file yaml
+    epochs=50,
     imgsz=640,
     batch=16,
-    device='cpu',  # o '0' se hai GPU Nvidia
-    name='cr_custom',  # nome cartella output
+    device='0',  # o '0' se hai GPU Nvidia
+    name='dataset2_50iter',  # nome cartella output
+)
+model.train(
+    data='../dataset/2/data.yaml',  # percorso al file yaml
+    epochs=200,
+    imgsz=640,
+    batch=16,
+    device='0',  # o '0' se hai GPU Nvidia
+    name='dataset2_200iter',  # nome cartella output
+)
+model.train(
+    data='../dataset/3/data.yaml',  # percorso al file yaml
+    epochs=50,
+    imgsz=640,
+    batch=16,
+    device='0',  # o '0' se hai GPU Nvidia
+    name='dataset3_50iter',  # nome cartella output
+)
+model.train(
+    data='../dataset/3/data.yaml',  # percorso al file yaml
+    epochs=200,
+    imgsz=640,
+    batch=16,
+    device='0',  # o '0' se hai GPU Nvidia
+    name='dataset3_200iter',  # nome cartella output
 )
