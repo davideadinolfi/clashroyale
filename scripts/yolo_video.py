5dc78c68794ef75d1bbb0a834cc79210e76d6df1
davideadinolfi <davideadinolfi01@gmail.com>
2026-02-17 17:54:10 +0100

    aiuto

--- scripts/yolo_video.py
+++ scripts/yolo_video.py
@@ -88,6 +88,6 @@ def process_video_basic(video_path, model_path, output_path):
 # Uso
 process_video_basic(
     video_path=Path(r"C:\Users\Davide Adinolfi\Downloads\Video senza titolo - Realizzato con Clipchamp (4).mp4"),
-    model_path="runs/detect/generato3/weights/best.pt",
+    model_path="runs/detect/generato4/weights/last.pt",
     output_path="runs/video/video_output.mp4"
 )
